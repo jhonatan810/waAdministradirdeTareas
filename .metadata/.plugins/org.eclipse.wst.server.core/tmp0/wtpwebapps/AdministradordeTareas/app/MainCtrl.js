21de9576d8f8b521b9e74367e03abbe4872b1911
jhonatan810 <jhonatan8100@gmail.com>
2016-11-18 16:30:46 -0600

    Proyecto
    Proyecto

---
+++ .metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/AdministradordeTareas/app/MainCtrl.js
@@ -0,0 +1,14 @@
+(function(){
+	"use strict";
+	
+	angular.module("AdministradordeTareas")
+		.controller("MainCtrl", [MainCtrl]);
+	
+	function MainCtrl(){
+		var vm = this;
+		
+		vm.saludo = "Hola que tal";
+		vm.despedida = "adios";
+	}
+	
+})();
